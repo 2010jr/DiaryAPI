9afef56c6d266ff00d62194a80405e54fa589197
Jun <jun.kusahana25@gmail.com>
2017-10-30 17:39:09 +0900

    Modify application and package settings

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 'use strict'
+
 var http = require('http');
 var express = require('express');
 var cfEnv = require('cfenv');
 
-var routes = require('./routes');
+var routes = require('./routes/routes');
 var app = express();
 var appEnv = cfEnv.getAppEnv();
 var server = http.createServer(app);
 var port = appEnv && appEnv.port ? appEnv.port : 3000;
+
 routes.router(app, server);
 server.listen(port);
 
